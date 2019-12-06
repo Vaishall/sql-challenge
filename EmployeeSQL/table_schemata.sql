8c1189845e14f0e3153e07a10682c779c4b7712e
Vaishall Pradeepkumar <mastervaishall@gmail.com>
2019-12-06 16:45:13 -0600

    added ERD and table schemata sql file

---
+++ EmployeeSQL/table_schemata.sql
@@ -0,0 +1,91 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "departments" (
+    "id" SERIAL   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "id" SERIAL   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    "from_date" DATE   NULL,
+    "to_date" DATE   NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "id" SERIAL   NOT NULL,
+    "dept_no" INT   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "from_date" DATE   NULL,
+    "to_date" DATE   NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "employees" (
+    "id" SERIAL   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "birth_date" DATE   NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
+    "hire_date" DATE   NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "id" SERIAL   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    "from_date" DATE   NULL,
+    "to_date" DATE   NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "titles" (
+    "id" SERIAL   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "from_date" DATE   NULL,
+    "to_date" DATE   NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "id"
+     )
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
